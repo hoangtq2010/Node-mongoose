8c4c948194717211e7d519e8c6c23d4c9f0b70ff
hoang <36702685+hoangtq2010@users.noreply.github.com>
2021-04-19 17:01:30 +0300

    Mongoose Part 3

--- index.js
+++ index.js
@@ -8,23 +8,40 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    //var newDish = Dishes({
+    
     Dishes.create({
-        name : 'Happizza',
+        name : 'Uthapizza',
         description : 'test'
     })
 
-    //newDish.save()      //tao nhanh object
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();  //exec: dam bao dieu nay dc thuc hien, tra lai promise 
+        return Dishes.findByIdAndUpdate(dish._id, {     //dishId đề cập đến dish vừa chèn và sửa đổi $set:{}
+            $set : {description : 'Updated test'}
+        },{
+            new : true              //sau khi update sẽ trả về dish đã update
+        })
+        .exec();  //exec: dam bao dieu nay dc thuc hien, tra lai promise 
     })
-    .then((dishes) => {
-        console.log(dishes);
 
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({        //đẩy 1 document comment vào dish
+            rating : 5,
+            comment : ' I\'m getting a sinking feeling!',
+            author : 'Messi'
+        });
+
+        return dish.save();         //trả lại dish cho callback tiếp
+    })
+    
+    .then((dish) => {
+        console.log(dish);
         return Dishes.remove({});
     })
+    
     .then(() => {
         return mongoose.connection.close();
     })
